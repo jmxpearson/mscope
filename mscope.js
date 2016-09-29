743e8c801bddc9bc3355c54ace46d51353016fac
John Pearson <jmxpearson@gmail.com>
2016-09-29 16:02:32 -0400

    cleaning up

--- mscope.js
+++ mscope.js
@@ -1,5 +1,13 @@
 "use strict"
 
+// OpenEphys record:
+// - int64: timestamp: 8 (little)
+// - uint16: number of samples in record: 2 (little)
+// - uint16: recording number: 2 (little)
+// - int16 * 1024: samples: 2048 (big)
+// - uint8 * 10: 10-byte record end code [0, 1, 2, 3, 4, 5, 6, 7, 8, 255]: 10
+// total:
+const RECORD_LENGTH = 2070;
 const HEADER_LENGTH = 1024;
 
 function handleFileSelect(evt) {
@@ -17,6 +25,7 @@ function handleFileSelect(evt) {
 
     for (let i = 0, f; f = files[i]; i++) {
         let reader = new FileReader();
+        let recReader = new FileReader();
 
         reader.onloadend = function(evt) {
             let buff = evt.target.result;
@@ -29,7 +38,14 @@ function handleFileSelect(evt) {
 
         reader.readAsArrayBuffer(f.slice(0, HEADER_LENGTH));
 
-        readRecord(f);
+        recReader.onloadend = function(evt) {
+            let buff = evt.target.result;
+            console.log(buff.byteLength)
+            readRecords(buff, 0, 10);
+        }
+
+        let numrecs = 10;
+        recReader.readAsArrayBuffer(f.slice(HEADER_LENGTH, HEADER_LENGTH + (numrecs + 1) * RECORD_LENGTH));
 
     }
 
@@ -37,24 +53,14 @@ function handleFileSelect(evt) {
 
 document.getElementById('files').addEventListener('change', handleFileSelect, false);
 
-function readRecord(theFile, recnum=0) {
-    // take an OpenEphys .continuous file object and read a given record
-    // OpenEphys record:
-    // - int64: timestamp: 8 (little)
-    // - uint16: number of samples in record: 2 (little)
-    // - uint16: recording number: 2 (little)
-    // - int16 * 1024: samples: 2048 (big)
-    // - uint8 * 10: 10-byte record end code [0, 1, 2, 3, 4, 5, 6, 7, 8, 255]: 10
-    // total:
-    const RECORD_LENGTH = 2070;
+function readRecords(buff, startrecord=0, numrecs=1) {
+    // take an OpenEphys .continuous file buffer object and read a given record
+    let recarray = new Array(numrecs);
 
-    let reader = new FileReader();
-    reader.onloadend = function(evt) {
-        let buff = evt.target.result;
-        console.log(buff.byteLength)
-
+    for (let r = 0; r < numrecs; r++) {
         let rec = {}
-        let view = new DataView(buff, 0, 12);
+        let offset = (r + startrecord) * RECORD_LENGTH;
+        let view = new DataView(buff, offset, 12);
         rec.tstamp = 'foo';
         rec.nsamples = view.getUint16(8, true);
         rec.recordingNumber = view.getUint16(10, true);
@@ -62,15 +68,17 @@ function readRecord(theFile, recnum=0) {
 
         // now read in samples: need to preserve endianness
         rec.samples = new Int16Array(1024);
-        let source = new DataView(buff, 12, 2048);  // source bytes
+        let source = new DataView(buff, offset + 12, 2048);  // source bytes
         let sink = new DataView(rec.samples.buffer);
         for (let i = 0; i < source.byteLength; i += 2) {
             sink.setInt16(i, source.getInt16(i, false));
         }
 
-        console.log(rec);
+        recarray[r] = rec;
+
     }
 
-    reader.readAsArrayBuffer(theFile.slice(HEADER_LENGTH, HEADER_LENGTH + (recnum + 1) * RECORD_LENGTH));
+
+    console.log(recarray);
 
 }
